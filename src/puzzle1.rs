494a9615c9ab3a30e485a2f0c5e2834a06361305
ncoif <github@ncoif.net>
2018-12-02 13:04:29 +0000

    change visibility

--- src/puzzle1.rs
+++ src/puzzle1.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-pub(crate) fn run() {
+pub fn run() {
     let mut current_frequency = 0;
     let mut seen = HashSet::new();
     let mut first_seen = None;
