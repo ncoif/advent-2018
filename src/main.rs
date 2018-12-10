00af1b2294a3131cbc3c9c5f34bb1bc85d5e2d31
ncoif <github@ncoif.net>
2018-12-10 22:02:13 +0000

    rename files for easier readability

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use std::env;
 
-mod puzzle1;
-mod puzzle10;
-mod puzzle2;
-mod puzzle3;
-mod puzzle4;
-mod puzzle5;
-mod puzzle6;
-mod puzzle7;
-mod puzzle8;
-mod puzzle9;
+mod puzzle01_frequency;
+mod puzzle02_checksum;
+mod puzzle03_fabric;
+mod puzzle04_nightshift;
+mod puzzle05_polymer;
+mod puzzle06_coords;
+mod puzzle07_dependencies;
+mod puzzle08_tree;
+mod puzzle09_marble;
+mod puzzle10_stars;
 
 fn main() {
     let day: u32 = env::args()
@@ -25,34 +25,34 @@ fn main() {
         .expect("invalid problem");
 
     match day * 10 + problem {
-        11 => puzzle1::answer1(),
-        12 => puzzle1::answer2(),
+        11 => puzzle01_frequency::answer1(),
+        12 => puzzle01_frequency::answer2(),
 
-        21 => puzzle2::answer1(),
-        22 => puzzle2::answer2(),
+        21 => puzzle02_checksum::answer1(),
+        22 => puzzle02_checksum::answer2(),
 
-        31 => puzzle3::answer1(),
-        32 => puzzle3::answer2(),
+        31 => puzzle03_fabric::answer1(),
+        32 => puzzle03_fabric::answer2(),
 
-        41 => puzzle4::answer1(),
-        42 => puzzle4::answer2(),
+        41 => puzzle04_nightshift::answer1(),
+        42 => puzzle04_nightshift::answer2(),
 
-        51 => puzzle5::answer1(),
-        52 => puzzle5::answer2(),
+        51 => puzzle05_polymer::answer1(),
+        52 => puzzle05_polymer::answer2(),
 
-        61 => puzzle6::answer1(),
-        62 => puzzle6::answer2(),
+        61 => puzzle06_coords::answer1(),
+        62 => puzzle06_coords::answer2(),
 
-        71 => puzzle7::answer1(),
-        72 => puzzle7::answer2(),
+        71 => puzzle07_dependencies::answer1(),
+        72 => puzzle07_dependencies::answer2(),
 
-        81 => puzzle8::answer1(),
-        82 => puzzle8::answer2(),
+        81 => puzzle08_tree::answer1(),
+        82 => puzzle08_tree::answer2(),
 
-        91 => puzzle9::answer1(),
-        92 => puzzle9::answer2(),
+        91 => puzzle09_marble::answer1(),
+        92 => puzzle09_marble::answer2(),
 
-        101 => puzzle10::answer1(),
+        101 => puzzle10_stars::answer1(),
 
         _ => panic!("Invalid problem day: {}, {}", day, problem),
     }
