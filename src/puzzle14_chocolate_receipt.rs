55e5a7135fa9d953f21d4222c5bb665b0b2552f5
ncoif <github@ncoif.net>
2018-12-16 17:43:06 +0000

    day 14 only check the latest 10 elements

--- src/puzzle14_chocolate_receipt.rs
+++ src/puzzle14_chocolate_receipt.rs
@@ -42,33 +42,37 @@ fn simulate2(input: &[u8]) -> usize {
         elf2: 1,
     };
 
-    // to avoid any out of bound with .window
-    (0..input.len()).for_each(|_| s.step());
-
     let mut steps_count = input.len();
     loop {
-        let slice = &*s.list;
+        let slice = if s.list.len() > 10 {
+            // only check the latest 10 elements
+            &s.list[(s.list.len() - 10)..]
+        } else {
+            &*s.list
+        };
         if slice.windows(input.len()).any(|s| s == input) {
             break;
         }
         s.step();
-        steps_count += 1;
 
-        if steps_count % 10000 == 0 {
+        steps_count += 1;
+        if steps_count % 1000_000 == 0 {
             println!("Tried {} steps so far", steps_count);
         }
     }
 
-    s.list.windows(input.len()).position(|s| s == input).unwrap()
+    s.list
+        .windows(input.len())
+        .position(|s| s == input)
+        .unwrap()
 }
-
 
 pub fn answer1() {
     println!("answer1: {:?}", simulate1(580741));
 }
 
 pub fn answer2() {
-    println!("answer1: {:?}", simulate2(&vec![5,8,0,7,4,1]));
+    println!("answer1: {:?}", simulate2(&vec![5, 8, 0, 7, 4, 1]));
 }
 
 #[test]
@@ -81,8 +85,8 @@ fn test_simulate1() {
 
 #[test]
 fn test_simulate2() {
-    assert_eq!(simulate2(&vec![5,1,5,8,9]), 9);
-    assert_eq!(simulate2(&vec![0,1,2,4,5]), 5);
-    assert_eq!(simulate2(&vec![9,2,5,1,0]), 18);
-    assert_eq!(simulate2(&vec![5,9,4,1,4]), 2018);
+    assert_eq!(simulate2(&vec![5, 1, 5, 8, 9]), 9);
+    assert_eq!(simulate2(&vec![0, 1, 2, 4, 5]), 5);
+    assert_eq!(simulate2(&vec![9, 2, 5, 1, 0]), 18);
+    assert_eq!(simulate2(&vec![5, 9, 4, 1, 4]), 2018);
 }
