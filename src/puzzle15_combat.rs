2bce5af3b2a34ec75eb3c3eaa3b871acb760491d
ncoif <github@ncoif.net>
2018-12-17 00:23:06 +0000

    make dijkstra_all work

--- src/puzzle15_combat.rs
+++ src/puzzle15_combat.rs
@@ -19,6 +19,9 @@ struct State {
     units: Vec<Unit>,
 }
 
+#[derive(Debug, PartialEq, Eq, Copy, Clone, Hash)]
+struct Node(usize, usize); // (x,y)
+
 impl State {
     fn parse(s: &str) -> State {
         let lines = s.split("\n");
@@ -33,11 +36,11 @@ impl State {
                     '.' => wall.push(false),
                     'G' => {
                         wall.push(false);
-                        units.push(Unit::new(x, y, false, 300));
+                        units.push(Unit::new(x, y, false, 200));
                     }
                     'E' => {
                         wall.push(false);
-                        units.push(Unit::new(x, y, true, 300));
+                        units.push(Unit::new(x, y, true, 200));
                     }
                     _ => panic!("Unexpected token"),
                 }
@@ -45,7 +48,40 @@ impl State {
             walls.push(wall);
         }
 
+        // sort units by turn order
+        units.sort_by_key(|u| (u.y, u.x));
+
         State { walls, units }
+    }
+
+    fn around(n: Node) -> impl Iterator<Item = Node> {
+        [(-1isize, 0isize), (1, 0), (0, -1), (0, 1)]
+            .iter()
+            .map(move |(dx, dy)| (n.0 as isize + dx, n.1 as isize + dy))
+            .map(|(x, y)| Node(x as usize, y as usize))
+    }
+
+    fn is_free(&self, n: &Node) -> bool {
+        !self.walls[n.1][n.0]
+            && self
+                .units
+                .iter()
+                .find(|u| u.x == n.0 && u.y == n.1)
+                .is_none()
+    }
+
+    // use dijkstra_all to find the best node
+    fn find_target(&self, start: &Node) -> Option<Node> {
+        let all_reachables = pathfinding::directed::dijkstra::dijkstra_all(start, |n| {
+            // cannot collect the iterator at any point here, as it will be collected by dijkstra_all
+            // or else "temporary value moved while borrowing" error
+            Self::around(n.clone())
+                .filter(|n| self.is_free(n))
+                .map(|n| n.clone())
+                .map(|n| (n, 1)) // cost of 1
+        });
+        println!("all_reachables: {:?}", all_reachables);
+        None
     }
 }
 
@@ -103,4 +139,20 @@ fn test_parse() {
     assert_eq!(state.units.len(), 4);
     assert_eq!(state.walls.len(), 5);
     assert_eq!(state.walls[0].len(), 7);
+}
+
+#[test]
+fn test_find_target() {
+    let state = State::parse(
+        r#"
+#######
+#E..G.#
+#...#.#
+#.G.#G#
+#######"#,
+    );
+    println!("{:?}", state);
+
+    let target = state.find_target(&Node(1, 1));
+    assert_eq!(target, Some(Node(4, 1)));
 }
