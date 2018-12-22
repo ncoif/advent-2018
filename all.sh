1cb6b53b0897399339885571351dc6586eba97f1
ncoif <github@ncoif.net>
2018-12-22 21:17:30 +0100

    Add script to run all solutions

---
+++ all.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+cargo build --release
+
+time target/release/aoc 1 1
+time target/release/aoc 1 2
+
+time target/release/aoc 2 1
+time target/release/aoc 2 2
+
+time target/release/aoc 3 1
+time target/release/aoc 3 2
+
+time target/release/aoc 4 1
+time target/release/aoc 4 2
+
+time target/release/aoc 5 1
+time target/release/aoc 5 2
+
+time target/release/aoc 6 1
+time target/release/aoc 6 2
+
+time target/release/aoc 7 1
+time target/release/aoc 7 2
+
+time target/release/aoc 8 1
+time target/release/aoc 8 2
+
+time target/release/aoc 9 1
+time target/release/aoc 9 2
+
+time target/release/aoc 10 1
+
+time target/release/aoc 11 1
+time target/release/aoc 11 2
+
+time target/release/aoc 12 1
+time target/release/aoc 12 2
+
+time target/release/aoc 13 1
+time target/release/aoc 13 2
+
+time target/release/aoc 14 1
+time target/release/aoc 14 2
+
+time target/release/aoc 15 1
+time target/release/aoc 15 2
