40bcca8e233e97826a867dac1124088d3f08cf7d
ncoif <github@ncoif.net>
2018-12-28 19:29:33 +0100

    day 23 answer 1

--- src/puzzle23_nanobots.rs
+++ src/puzzle23_nanobots.rs
@@ -5,12 +5,12 @@ use std::io::{BufRead, BufReader};
 use std::num::ParseIntError;
 use std::str::FromStr;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
 struct Nanobot {
     x: i64,
     y: i64,
     z: i64,
-    r: i64,
+    r: u64,
 }
 
 impl FromStr for Nanobot {
@@ -29,7 +29,7 @@ impl FromStr for Nanobot {
         let x: i64 = c[1].parse().unwrap();
         let y: i64 = c[2].parse().unwrap();
         let z: i64 = c[3].parse().unwrap();
-        let r: i64 = c[4].parse().unwrap();
+        let r: u64 = c[4].parse().unwrap();
 
         Ok(Nanobot { x, y, z, r })
     }
@@ -39,10 +39,18 @@ impl Nanobot {
     fn distance_to(&self, other: &Nanobot) -> u64 {
         ((self.x - other.x).abs() + (self.y - other.y).abs() + (self.z - other.z).abs()) as u64
     }
+
+    // a copy of all the nanobots in range from the given list
+    fn in_range(&self, others: &Vec<Nanobot>) -> Vec<Nanobot> {
+        others
+            .iter()
+            .filter(|n| self.distance_to(&n) <= self.r)
+            .map(|n| n.clone())
+            .collect()
+    }
 }
 
-fn read_file() -> Vec<Nanobot> {
-    let filename = "input/input23.txt";
+fn read_file(filename: &str) -> Vec<Nanobot> {
     let file = File::open(filename).expect("cannot open file");
     let reader = BufReader::new(file);
 
@@ -55,11 +63,14 @@ fn read_file() -> Vec<Nanobot> {
 }
 
 pub fn answer1() {
-    let nanobots = read_file();
+    let nanobots = read_file("input/input23.txt");
+
+    let max_radius_nanobot = nanobots.iter().max_by_key(|n| n.r).unwrap();
+    let in_range = max_radius_nanobot.in_range(&nanobots);
 
     println!(
         "Experimental Emergency Teleportation (1/2): {:?}",
-        nanobots.len()
+        in_range.len()
     );
 }
 
@@ -94,4 +105,18 @@ fn test_nanobot_distance() {
         r: 0,
     };
     assert_eq!(5, origin.distance_to(&n));
+}
+
+#[test]
+fn test_in_range() {
+    let nanobots = read_file("input/input23_debug.txt");
+
+    let max_radius_nanobot = nanobots.iter().max_by_key(|n| n.r).unwrap();
+    let in_range = max_radius_nanobot.in_range(&nanobots);
+
+    println!("max_radius_nanobot: {:?}", max_radius_nanobot);
+    println!("in_range: {:?}", in_range);
+
+    assert_eq!(4, max_radius_nanobot.r);
+    assert_eq!(7, in_range.len());
 }
