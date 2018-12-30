815c7588bb2cc6c7d19f67a62bcf8d8cfe046ce6
ncoif <github@ncoif.net>
2018-12-30 03:44:34 +0100

    day 24 work-in-progress

--- src/puzzle24_army.rs
+++ src/puzzle24_army.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
 enum Element {
     FIRE,
     COLD,
@@ -6,7 +9,9 @@ enum Element {
     RADIATION,
 }
 
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
 struct Army {
+    id: u64,
     infection: bool,
     size: u64,
     hit_points: u64,
@@ -15,6 +20,82 @@ struct Army {
     attack: u64,
     attack_type: Element,
     initiative: u64,
+    is_targeted: bool,
+}
+
+impl Army {
+    fn effective_power(&self) -> u64 {
+        self.size * self.attack
+    }
+
+    fn damage_to(&self, other: &Army) -> u64 {
+        if other.immune.contains(&self.attack_type) {
+            return 0;
+        }
+
+        if other.weak.contains(&self.attack_type) {
+            return 2 * self.effective_power();
+        }
+
+        self.effective_power()
+    }
+
+    fn deal_damage(&self, other: &mut Army) {
+        let damage = self.damage_to(&other);
+        let unit_killed: u64 = damage / other.hit_points;
+        other.size -= unit_killed;
+        println!("{:?} deals {} damage to {:?}", self, damage, other);
+    }
+}
+
+fn combat_to_death(armies: &mut Vec<Army>) {
+    loop {
+        combat_turn(armies);
+
+        let immune_count = armies.iter().filter(|a| !a.infection).count();
+        let infection_count = armies.iter().filter(|a| a.infection).count();
+
+        if immune_count == 0 || infection_count == 0 {
+            break;
+        }
+    }
+}
+
+fn combat_turn(armies: &mut Vec<Army>) {
+    // targeting order
+    let mut targets = HashMap::new();
+    armies.sort_by_key(|a| (a.effective_power(), a.initiative));
+
+    //FIXME:
+    for army in armies {
+        let candidate = armies
+            .iter()
+            .filter(|a| a.infection != army.infection)
+            .filter(|a| army.damage_to(&a) > 0)
+            .filter(|a| a.is_targeted == true)
+            .max_by_key(|a| army.damage_to(&a));
+
+        println!("{:?} will target {:?}", army, candidate);
+        if candidate.is_some() {
+            let target = candidate.unwrap();
+            targets.insert(army.id, target.id);
+            target.is_targeted = true;
+        }
+    }
+
+    // combat order
+    armies.sort_by_key(|a| a.initiative);
+    for army in armies {
+        let target_id = targets.get(&army.id);
+        if target_id.is_some() {
+            let mut target = armies.iter().find(|a| a.id == *target_id.unwrap()).unwrap();
+            army.deal_damage(&mut target);
+            // reset target
+            target.is_targeted = false;
+        }
+    }
+
+    armies.retain(|a| a.size > 0);
 }
 
 fn debug_army() -> Vec<Army> {
@@ -22,6 +103,7 @@ fn debug_army() -> Vec<Army> {
     let mut armies = vec![];
 
     armies.push(Army {
+        id: 1,
         infection: false,
         size: 17,
         hit_points: 5390,
@@ -30,9 +112,11 @@ fn debug_army() -> Vec<Army> {
         attack: 4507,
         attack_type: FIRE,
         initiative: 2,
+        is_targeted: false,
     });
 
     armies.push(Army {
+        id: 2,
         infection: false,
         size: 989,
         hit_points: 1274,
@@ -41,9 +125,11 @@ fn debug_army() -> Vec<Army> {
         attack: 25,
         attack_type: SLASHING,
         initiative: 3,
+        is_targeted: false,
     });
 
     armies.push(Army {
+        id: 3,
         infection: true,
         size: 801,
         hit_points: 4706,
@@ -52,9 +138,11 @@ fn debug_army() -> Vec<Army> {
         attack: 116,
         attack_type: BLUDGEONING,
         initiative: 1,
+        is_targeted: false,
     });
 
     armies.push(Army {
+        id: 4,
         infection: true,
         size: 4485,
         hit_points: 2961,
@@ -63,6 +151,7 @@ fn debug_army() -> Vec<Army> {
         attack: 12,
         attack_type: SLASHING,
         initiative: 4,
+        is_targeted: false,
     });
 
     armies
@@ -72,4 +161,12 @@ pub fn answer1() {
     let armies = debug_army();
 
     println!("Immune System Simulator 20XX (1/2): {:?}", 0);
+}
+
+#[test]
+fn test_combat() {
+    let mut armies = debug_army();
+    combat_to_death(&mut armies);
+
+    assert_eq!(true, false);
 }
