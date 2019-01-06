b8519828c7f5e5478a3d024385163aae6cfe94a4
ncoif <github@ncoif.net>
2019-01-06 19:48:54 +0000

    all.sh

---
+++ all.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+cargo build --release
+
+time target/release/all 1 1
+time target/release/all 1 2
+
+time target/release/all 2 1
+time target/release/all 2 2
+
+time target/release/all 3 1
+time target/release/all 3 2
+
+time target/release/all 4 1
+time target/release/all 4 2
+
+time target/release/all 5 1
+time target/release/all 5 2
+
+time target/release/all 6 1
+time target/release/all 6 2
+
+time target/release/all 7 1
+time target/release/all 7 2
+
+time target/release/all 8 1
+time target/release/all 8 2
+
+time target/release/all 9 1
+time target/release/all 9 2
+
+time target/release/all 10 1
+
+time target/release/all 11 1
+time target/release/all 11 2
+
+time target/release/all 12 1
+time target/release/all 12 2
+
+time target/release/all 13 1
+time target/release/all 13 2
+
+time target/release/all 14 1
+time target/release/all 14 2
+
+time target/release/all 15 1
+time target/release/all 15 2
+
+time target/release/all 16 1
+time target/release/all 16 2
+
+time target/release/all 17 1
+time target/release/all 17 2
+
+time target/release/all 18 1
+time target/release/all 18 2
+
+time target/release/all 19 1
+time target/release/all 19 2
+
+time target/release/all 20 1
+time target/release/all 20 2
+
+time target/release/all 21 1
+time target/release/all 21 2
+
+time target/release/all 22 1
+time target/release/all 22 2
+
+time target/release/all 23 1
+#time target/release/all 23 2 // require z3
+
+time target/release/all 24 1
+time target/release/all 24 2
+
+time target/release/all 25 1
