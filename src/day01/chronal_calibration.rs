b8405848fe2f3b304f9f563a7742e5ce11621c16
ncoif <github@ncoif.net>
2019-01-06 04:13:09 +0000

    day01 improve performance

--- src/day01/chronal_calibration.rs
+++ src/day01/chronal_calibration.rs
@@ -9,31 +9,15 @@ use std::io::{BufRead, BufReader};
 type Frequency = i32;
 
 pub fn answer1() -> Result<AocResponse<Frequency>, AocError> {
-    let mut current_frequency = 0;
-    let mut seen = HashSet::new();
-    let mut first_seen = None;
-    let mut final_frequency = None;
-
     let inputs = read_file()?;
-    while first_seen.is_none() {
-        for input in &inputs {
-            current_frequency += input;
+    let final_frequency = inputs.iter().sum();
 
-            if first_seen.is_none() {
-                if seen.contains(&current_frequency) {
-                    first_seen = Some(current_frequency);
-                }
-                seen.insert(current_frequency);
-            }
-        }
-
-        final_frequency.get_or_insert(current_frequency);
-    }
-
-    match final_frequency {
-        None => Err(AocError::ComputeNotFound),
-        Some(freq) => Ok(AocResponse::new(1, 2, "Chronal Calibration", freq)),
-    }
+    Ok(AocResponse::new(
+        1,
+        2,
+        "Chronal Calibration",
+        final_frequency,
+    ))
 }
 
 pub fn answer2() -> Result<AocResponse<Frequency>, AocError> {
@@ -42,17 +26,14 @@ pub fn answer2() -> Result<AocResponse<Frequency>, AocError> {
     let mut first_seen = None;
 
     let inputs = read_file()?;
-    while first_seen.is_none() {
-        for input in &inputs {
-            current_frequency += input;
+    for input in inputs.iter().cycle() {
+        current_frequency += input;
 
-            if first_seen.is_none() {
-                if seen.contains(&current_frequency) {
-                    first_seen = Some(current_frequency);
-                }
-                seen.insert(current_frequency);
-            }
+        if seen.contains(&current_frequency) {
+            first_seen = Some(current_frequency);
+            break;
         }
+        seen.insert(current_frequency);
     }
 
     match first_seen {
