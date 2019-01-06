dcad2191a25d8b5a09ff3de9787e64fb4d527a47
ncoif <github@ncoif.net>
2019-01-06 04:56:00 +0000

    some tidying up

--- src/day01/chronal_calibration.rs
+++ src/day01/chronal_calibration.rs
@@ -4,7 +4,6 @@ use crate::common::response::AocResponse;
 use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-//use test::{black_box, Bencher};
 
 type Frequency = i32;
 
@@ -68,19 +67,3 @@ fn test_answer2() {
     let answer = answer2().unwrap();
     assert_eq!(answer.get(), 72889);
 }
-
-// #[bench]
-// #[allow(unused_must_use)]
-// fn bench_answer1(b: &mut Bencher) {
-//     b.iter(|| {
-//         black_box(answer1());
-//     });
-// }
-
-// #[bench]
-// #[allow(unused_must_use)]
-// fn bench_answer2(b: &mut Bencher) {
-//     b.iter(|| {
-//         black_box(answer2());
-//     });
-// }
